59bf88a220635ee506cc7c16afa7bd4598b91042
chandrika-collab <chandrikathippani@gmail.com>
2026-07-19 09:59:59 -0500

    Update Stock.java
    added a INT

--- Stock.java
+++ Stock.java
@@ -24,7 +24,8 @@ public class Stock {
     private int issueFix; //add a new private variable issueFix
     private Date tradingDate;
     private LocalDate tradingDate;
-    private int pricePercentage
+    private int pricePercentage;
+    private int num;
 
 
     public Stock(String tickerSymbol,
