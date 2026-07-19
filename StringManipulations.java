2cd8dd16e441d6d6fd13acc6544c5e2166d51d25
keerthireddyboggula <80941230+keerthireddyboggula@users.noreply.github.com>
2026-07-19 18:54:41 -0500

    Update StringManipulations.java
    changed the name

--- StringManipulations.java
+++ StringManipulations.java
@@ -5,7 +5,7 @@ public class StringManipulations {
     public static void main(String[] args) {
         String str = "test";
         str = "test" + "string";
-        string sh= "shourya"
+        string sh= "keerthi"
         System.out.println("String length: " + str.length());
         System.out.println("String to upper: " + str.toUpperCase());
 
