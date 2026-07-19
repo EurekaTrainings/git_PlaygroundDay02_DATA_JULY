fa75e5aa393b41607b8bf7fbd9d0d7b8ddce49f3
DATTA SAI V V N <dvvn@uic.edu>
2026-07-19 18:17:16 -0500

    This is the commit 1 from Datta-Branch-1

--- Stock.java
+++ Stock.java
@@ -49,4 +49,22 @@ public class Stock {
     }
 
 }
-// test
+
+
+// Commit 1 From "Datta-Branch-1"
+//private int openPrice;
+//private int closePrice;
+//private Date  tradingdate;
+//private double currentRatio;
+//private int openprice;
+//private int closeprice;
+//private Date tradingdate;
+//private LocalDate tradingdate;
+//private string strrr;
+//private int openPrice;
+//private int closePrice;
+//private int issueFix; //add a new private variable issueFix
+//private Date tradingDate;
+//private LocalDate tradingDate;
+//private int pricePercentage;
+//private int num;
