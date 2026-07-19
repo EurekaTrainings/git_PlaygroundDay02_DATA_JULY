3500d722732fb102b5e7de4dc3f5f81d4263ccf4
Sairam1503 <maddelasairamgufus2023@gmail.com>
2026-07-19 09:31:21 -0500

    added the date to stock.java

--- Stock.java
+++ Stock.java
@@ -7,7 +7,7 @@ public class Stock {
     private int marketCap;
     private int openprice;
     private int closeprice;
-    
+    private LocalDate tradingdate;
 
 
     public Stock(String tickerSymbol,
