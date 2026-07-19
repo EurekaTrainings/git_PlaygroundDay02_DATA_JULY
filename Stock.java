f78d1bb29b373ddbba0ba69d017f22023b578302
Sreeshma <51902280+SREESHMA-1429@users.noreply.github.com>
2026-07-19 18:57:27 -0500

    Update Stock.java
    Add a string sector ID

--- Stock.java
+++ Stock.java
@@ -30,6 +30,7 @@ public class Stock {
     private int branch1; // 1st commit
      private int priceclose;
     private String amazon_1;
+    private string sectorid;
 
 
     public Stock(String tickerSymbol,
