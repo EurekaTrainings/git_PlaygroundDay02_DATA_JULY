2e281100660e1e66bee1c8afd817b5b33ff4d089
Vardhan Are <vardhanare1998@gmail.com>
2026-07-19 19:49:59 -0500

    This is the 1st commit from Branch 3

--- Stock.java
+++ Stock.java
@@ -26,7 +26,7 @@ public class Stock {
     private LocalDate tradingDate;
     private int pricePercentage;
     private int num;
-
+    private int branch3;//this is first commit in branch3
 
     public Stock(String tickerSymbol,
                  String tickerName,
