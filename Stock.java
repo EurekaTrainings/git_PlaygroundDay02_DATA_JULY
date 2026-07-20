f68b9c29ef1cfaf0d46688869b6c34d57df12dcb
keerthireddyboggula <keerthanareddy654@gmail.com>
2026-07-19 20:45:24 -0500

    removed the int

--- Stock.java
+++ Stock.java
@@ -38,7 +38,6 @@ public class Stock {
      private int priceclose;
     private String amazon_1;
     private int branch2.1; //this is 2nd commit in branch2
-    private int branch2.1.1;
 
     private int branch2.1;
     private int open1;//this is 2nd commit in branch2
