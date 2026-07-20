b18014159524ca6afde986c7ffceffb948b33377
Vardhan Are <vardhanare1998@gmail.com>
2026-07-19 19:21:00 -0500

    This is the 2nd commit from Branch 2

--- Stock.java
+++ Stock.java
@@ -32,6 +32,8 @@ public class Stock {
     private int avgprice;
      private int priceclose;
     private String amazon_1;
+    private int branch2.1; //this is 2nd commit in branch2
+
 
 
     public Stock(String tickerSymbol,
