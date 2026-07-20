4e3baec903548b1a96ea726c92db1ce6f492e50c
pawankalyansampi <nagswaroop249@gmail.com>
2026-07-20 09:28:30 -0500

    Added a statement swaroop4 in manipulations

--- StringManipulations.java
+++ StringManipulations.java
@@ -19,7 +19,7 @@ public class StringManipulations {
         System.out.println("New str for merge ");
         System.out.println("String starts with test: " + str.startsWith("test"));
         System.out.println("String ends with string: " + str.endsWith("string"));
-
+        System.out.println("Swaroop4 added to check");
         String data = "AAPL,MSFT,GOOG,TSLA,Fb,TechMahindra,Cognizant";
         String[] tickers = data.split(",");
         for (String ticker : tickers) {
