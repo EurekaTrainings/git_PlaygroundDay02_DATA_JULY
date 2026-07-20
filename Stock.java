33dc1e24dc15e569d8cd442bf0498fa7b67ebd2e
keerthireddyboggula <80941230+keerthireddyboggula@users.noreply.github.com>
2026-07-19 20:55:38 -0500

    Update Stock.java
    removed

--- Stock.java
+++ Stock.java
@@ -36,8 +36,7 @@ public class Stock {
     private int num1;
     private Date tradingyear;
      private int priceclose;
-    private String amazon_1;
-    private int branch2.1; //this is 2nd commit in branch2
+    private String amazon_1; //this is 2nd commit in branch2
 
     private int branch2.1;
     private int open1;//this is 2nd commit in branch2
